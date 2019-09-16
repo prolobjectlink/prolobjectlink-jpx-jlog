e005f886d38d10292f3008bb9eed92d96aca1ce6
Jose Zalacain <Jose Zalacain@DESKTOP-P1RGQ48>
2019-09-16 06:03:35 -0400

    Update contributing

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
+java -classpath "$(dirname "$(pwd)")/lib/*" org.prolobjectlink.db.prolog.jlog.JLogDatabaseConsole -m
 java -classpath "$(dirname "$(pwd)")/lib/*" org.prolobjectlink.db.prolog.jlog.JLogDatabaseConsole -z 9110
