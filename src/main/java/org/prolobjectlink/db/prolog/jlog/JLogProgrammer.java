4915471eb5efb36fa805733e8a47d9dda70f3032
Jose Zalacain <Jose Zalacain@DESKTOP-P1RGQ48>
2019-09-23 19:01:15 -0400

    Update pom.xml

--- src/main/java/org/prolobjectlink/db/prolog/jlog/JLogProgrammer.java
+++ src/main/java/org/prolobjectlink/db/prolog/jlog/JLogProgrammer.java
@@ -38,7 +38,7 @@ public final class JLogProgrammer extends AbstractProgrammer implements PrologPr
 		super(provider);
 	}
 
-	public void codingInclusion(PrintWriter programmer, String jarEntryName) {
+	public void codingObjInclusion(PrintWriter programmer, String jarEntryName) {
 		StringBuilder b = new StringBuilder();
 		for (int i = 0; i < jarEntryName.lastIndexOf('/'); i++) {
 			if (jarEntryName.charAt(i) == '/') {
@@ -46,6 +46,18 @@ public final class JLogProgrammer extends AbstractProgrammer implements PrologPr
 			}
 		}
 		b.append("../../obj/prolobject.pl");
+		programmer.println(":-" + provider.prologInclude("" + b + "") + ".");
+		programmer.println();
+	}
+
+	public void codingDaoInclusion(PrintWriter programmer, String jarEntryName, String dao) {
+		StringBuilder b = new StringBuilder();
+		for (int i = 0; i < jarEntryName.lastIndexOf('/'); i++) {
+			if (jarEntryName.charAt(i) == '/') {
+				b.append("../");
+			}
+		}
+		b.append("../../web/" + dao);
 		programmer.println(":-" + provider.prologInclude("" + b + "") + ".");
 		programmer.println();
 	}
